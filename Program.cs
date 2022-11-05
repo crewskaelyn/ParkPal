7b03c09e6a5dee8dcf8fc44538411e5a2e6c4a67
Logan Limbaugh <91567972+Loganhl@users.noreply.github.com>
2022-11-04 19:27:38 -0500

    Goals
    Comments Detailing Goals of the Program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Goal 1 - Create a database of the different parkings lots and streets we wish to use within our program.
+//Goal 2 - Create a program that marks the percentages of parking lots in regard to how full they are.
+//Goal 3 - Locations
+//Goal 4 - User Login Features
+//Goal 5 - Information attached to each parking lot, nearby businesses and food places.
+
+namespace tigerhacks{
+
+    class Main
+ {
+     static void Main(string[] args);
+}
